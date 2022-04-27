21fb3b49243c9026803d92fae6b30add9a27987e
Arif Kurniawan <52802394+arla27@users.noreply.github.com>
2022-04-27 09:23:24 +0700

    Modif#8

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Universi Insan Pembangunan'),
+        title: Text('Universit Insan Pembangunan'),
       ),
       // membuat objek dari kelas BottomNavigationBar
       bottomNavigationBar: BottomNavigationBar(
